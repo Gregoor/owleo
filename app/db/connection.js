6be04332142acc88f9b0976f73467a8d06ddb9a6
Gregor Weber <gregorwbr@gmail.com>
2015-03-19 15:52:51 +0100

    New version of neo4j-node library

--- app/db/connection.js
+++ app/db/connection.js
@@ -1,9 +1,11 @@
 let neo4j = require('neo4j');
 
 let connection = {
-	'db': new neo4j.GraphDatabase(process.env.NEO4J_HOST || 'http://localhost:7474'),
+	'db': new neo4j.GraphDatabase({
+		'url': process.env.NEO4J_HOST || 'http://localhost:7474'
+	}),
 	'query': (query, params) => new Promise((resolve) => {
-		connection.db.query(query, params, (err, data) => {
+		connection.db.cypher({query, params, 'lean': true}, (err, data) => {
 			if (err) console.error(err);
 			else resolve(data);
 		});
