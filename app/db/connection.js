ab04c8a5ce3722c6963d31f423479b378acb7941
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2015-04-06 21:55:38 +0200

    added dbHost to config. its now possible to authenticate to neo4j via http://user:pw@localhost:7474

--- app/db/connection.js
+++ app/db/connection.js
@@ -1,8 +1,9 @@
 let neo4j = require('neo4j');
+let config = require('../config.custom');
 
 let connection = {
 	'db': new neo4j.GraphDatabase({
-		'url': process.env.NEO4J_HOST || 'http://localhost:7474'
+		'url': config.dbHost || 'http://localhost:7474'
 	}),
 	'query': (query, params) => new Promise((resolve) => {
 		connection.db.cypher({query, params, 'lean': true}, (err, data) => {
