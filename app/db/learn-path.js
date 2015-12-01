1cdabfb19c0382c78327dfc5fd03899f60044e11
Gregor <gregorwbr@gmail.com>
2015-12-01 21:00:42 +0100

    include target concept in learn path

--- app/db/learn-path.js
+++ app/db/learn-path.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 const findLearnPath = (id) => {
   return query(
     `
-      MATCH (c:Concept {id: {id}})-[:REQUIRES|CONTAINED_BY*]->(reqs)
+      MATCH (c:Concept {id: {id}})-[:REQUIRES|CONTAINED_BY*0..]->(reqs)
       WITH COLLECT(reqs.id) AS reqs
       RETURN reqs
     `,
