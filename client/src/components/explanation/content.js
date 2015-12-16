987d5dd00f0067fc002e7e675b8f9831b6101b81
Gregor <gregorwbr@gmail.com>
2015-12-16 03:56:07 +0100

    switch from iframely to embedly

--- client/src/components/explanation/content.js
+++ client/src/components/explanation/content.js
@@ -1,35 +1,25 @@
 import React from 'react';
 import Relay from 'react-relay';
+import '../../../lib/embedly';
+
+import {Spinner} from '../mdl';
 
 import shortenURL from '../../helpers/shorten-url';
 
 class ExplanationContent extends React.Component {
 
-  componentDidMount() {
-    this._loadIframely();
-  }
-
-  componentDidUpdate() {
-    this._loadIframely();
-  }
-
   render() {
-    let {type, content} = this.props.explanation;
+    const {type, content} = this.props.explanation;
 
     let explanationContent, style;
     return type == 'link' ?
       <div className="explanation">
-        <a ref="link" data-iframely-url href={content}>
+        <a href={content} className="embedly-card" data-card-controls="0"
+           data-card-chrome="0">
           {content.length > 50 ? shortenURL(content) : content}
         </a>
       </div> :
       <div dangerouslySetInnerHTML={{__html: content}}/>;
-  }
-
-  _loadIframely() {
-    if (this.props.explanation.type == 'link') {
-      window.iframely.load(this.refs.link);
-    }
   }
 
 }
