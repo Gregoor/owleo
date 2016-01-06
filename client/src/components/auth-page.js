eaad4e8adf762511380ba3f63a6c1a9e08be9ca5
Gregor <gregorwbr@gmail.com>
2016-01-06 20:42:37 +0100

    AUTH PAGE CAN ONLY SERVE USABILITY WHEN STATE MACHININESS IS MAXIMIZED

--- client/src/components/auth-page.js
+++ client/src/components/auth-page.js
@@ -2,16 +2,23 @@ import React from 'react';
 import Relay from 'react-relay';
 import DocumentTitle from 'react-document-title';
 import {
-  Button, Card, CardActions, CardText, CardTitle, Checkbox, Textfield
+  Button, Card, CardActions, CardText, CardTitle, Checkbox, Icon,
+  ProgressBar, Spinner, Textfield
 } from 'react-mdl';
+import _ from 'lodash';
 
 import history from '../history';
 import LoginMutation from '../mutations/user/login';
 
+const Check = () => <Icon name="check_circle" style={{color: '#4CAF50'}}/>;
+const Cross = () => <Icon name="cancel" style={{color: '#DE3226'}}/>;
+
 class AuthPage extends React.Component {
 
   state = {
-    loginMode: true, authFailed: false, nameInvalid: true, pwInvalid: true
+    loginMode: true, isAuthenticating: false, authFailed: false,
+    nameEmpty: true, isCheckingName: false, pwEmpty: true, pwRepeatEmpty: true,
+    pwMismatch: undefined
   };
 
   componentWillMount() {
@@ -19,61 +26,98 @@ class AuthPage extends React.Component {
     this._handleFormChange = this._handleFormChange.bind(this);
     this._handleNameChange = this._handleNameChange.bind(this);
     this._handlePwChange = this._handlePwChange.bind(this);
+    this._handlePwRepeatChange = this._handlePwRepeatChange.bind(this);
+    this._handlePwRepeatBlur = this._handlePwRepeatBlur.bind(this);
     this._handleSubmit = this._handleSubmit.bind(this);
   }
 
   render() {
-    const {loginMode, authFailed, nameInvalid, pwInvalid} = this.state;
+    const {
+      loginMode, isAuthenticating, authFailed, isCheckingName, nameEmpty,
+      pwEmpty, pwRepeatEmpty, pwMismatch
+    } = this.state;
     const {userExists} = this.props.viewer;
 
-    const submitDisabled = (loginMode ? !userExists : userExists) ||
-      nameInvalid || pwInvalid || authFailed;
+    const isValidName = loginMode ? userExists : !userExists;
 
-    let userError;
-    if (loginMode && !userExists) userError = (
-      <div>
-        No user found with this name!
-        Do you want to <a href="#" onClick={this._handleChangeNew}>register</a>?
-      </div>
-    );
-    else if (!loginMode && userExists) userError = (
-      <div>
-        This name is already taken.
-        Do you want to <a href="#" onClick={this._handleChangeNew}>login</a>?
-      </div>
-    );
+    const submitDisabled = !loginMode && (pwRepeatEmpty || pwMismatch) ||
+      !isValidName || isAuthenticating || isCheckingName || pwEmpty ||
+      authFailed;
 
-    let errorText;
-    if (authFailed) errorText = (
-      <div>
-        <em style={{color: 'rgb(222, 50, 38)'}}>Authorization failed!</em>
-        <br/>
-      </div>
-    );
+    let nameError;
+    if (!nameEmpty && !isCheckingName) {
+      if (loginMode && !userExists) nameError = (
+        <div>
+          No user found with this name!
+          Did you want to&nbsp;
+          <a href="#" onClick={this._handleChangeNew} className="primary-color">
+            register
+          </a>?
+        </div>
+      );
+      else if (!loginMode && userExists) nameError = (
+        <div>
+          This name is already taken.
+          Did you want to&nbsp;
+          <a href="#" onClick={this._handleChangeNew} className="primary-color">
+            login
+          </a>?
+        </div>
+      );
+    }
+
+    let pwError;
+    if (authFailed) {
+      pwError = `That's not your password, ${this.refs.name.refs.input.value}!`
+    }
+
+    let pwRepeatError;
+    if (pwMismatch) {
+      pwRepeatError = 'Must be identical to password!';
+    }
 
     return (
       <DocumentTitle title="Auth">
         <form style={{marginBottom: 0}} onSubmit={this._handleSubmit}
               onChange={this._handleFormChange}>
-          <Card shadow={2} style={{margin: '11px auto'}}>
+          <Card shadow={2} style={{margin: '11px auto', maxWidth: 360}}>
             <CardTitle>Authentication</CardTitle>
             <CardText>
               <Textfield ref="name" label="Username" floatingLabel
-                         error={userError} onChange={this._handleNameChange}/>
-              <Textfield ref="password" label="Password" floatingLabel
-                         type="password" onChange={this._handlePwChange}/>
-              {loginMode ?
-                <div style={{height: 67}}/> :
-                <Textfield type="password" label="Repeat password"
-                           floatingLabel/>
+                         error={nameError} onChange={this._handleNameChange}
+                         style={{marginBottom: 10}}/>
+              {nameEmpty ? '' :
+                (isCheckingName ?
+                  <Spinner/> :
+                  (isValidName ? <Check key="check"/> : <Cross key="cross"/>))
               }
               <Checkbox label="I'm a new user" ripple checked={!loginMode}
                         onChange={this._handleChangeNew}/>
+              <Textfield ref="password" label="Password" floatingLabel
+                         type="password" error={pwError}
+                         onChange={this._handlePwChange}/>
+              {authFailed ? <Cross/> : (!loginMode && !pwEmpty ? <Check/> : '')}
+              {loginMode ?
+                <div style={{height: 67}}/> :
+                <div>
+                  <Textfield ref="passwordRepeat" type="password"
+                             label="Repeat password" floatingLabel
+                             onChange={this._handlePwRepeatChange}
+                             onBlur={this._handlePwRepeatBlur}
+                             error={pwRepeatError}/>
+                  {pwRepeatEmpty || pwMismatch === undefined ? '' :
+                    (pwMismatch ? <Cross/> : <Check/>)}
+                </div>
+              }
             </CardText>
             <CardActions>
               <Button type="submit" ripple accent disabled={submitDisabled}>
                 {loginMode ? 'Login' : 'Register'}
               </Button>
+              {isAuthenticating ?
+                <ProgressBar indeterminate style={{width: '100%'}}/> :
+                ''
+              }
             </CardActions>
           </Card>
         </form>
@@ -88,25 +132,46 @@ class AuthPage extends React.Component {
   _handleNameChange(event) {
     const {value} = event.target;
 
-    this.setState({nameInvalid: true});
-    if (value) this.props.relay.setVariables({name: value}, (readyState) => {
-      if (readyState.done) this.setState({nameInvalid: false});
-    });
+    this.setState({isCheckingName: true, nameEmpty: !value});
+    if (value) this._checkName(value);
   }
 
+  _checkName = _.debounce((name) => {
+    this.props.relay.setVariables({name}, (readyState) => {
+      if (readyState.done) this.setState({isCheckingName: false});
+    });
+  }, 300);
+
   _handlePwChange(event) {
-    this.setState({pwInvalid: !event.target.value});
+    this.setState({pwEmpty: !event.target.value});
+  }
+
+  _handlePwRepeatChange(event) {
+    const {value} = event.target;
+    this.setState({pwRepeatEmpty: !value});
+    this.setState({pwMismatch: undefined});
+  }
+
+  _handlePwRepeatBlur() {
+    const {password, passwordRepeat} = this.refs;
+    this.setState({
+      pwMismatch: passwordRepeat.refs.input.value != password.refs.input.value
+    });
   }
 
   _handleChangeNew(event) {
     const {checked} = event.target;
-    if (!checked) event.preventDefault();
-    this.setState({loginMode: !(checked || this.state.loginMode)});
+    if (checked === undefined) event.preventDefault();
+    this.setState({
+      loginMode: !(checked || this.state.loginMode),
+      authFailed: false, pwRepeatEmpty: true, pwMismatch: undefined
+    });
   }
 
   _handleSubmit(event) {
     event.preventDefault();
     const {name, password} = this.refs;
+    this.setState({isAuthenticating: true});
     Relay.Store.update(
       new LoginMutation({
         name: name.refs.input.value, password: password.refs.input.value
@@ -117,10 +182,10 @@ class AuthPage extends React.Component {
           location.reload();
         },
         onFailure: (t) => {
-          let {errors} = t.getError().source;
-          for (let {message} of errors) {
+          for (const {message} of t.getError().source.errors) {
             if (message == 'unauthorized') {
-              this.setState({authFailed: true});
+              this.setState({isAuthenticating: false, authFailed: true});
+              break;
             }
           }
         }
