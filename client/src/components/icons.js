63ea55213e3127fbcc0d6d6a7853503cd8d0e068
Gregor <gregorwbr@gmail.com>
2016-01-08 17:13:15 +0100

    move (in)valid colors into their own css class

--- client/src/components/icons.js
+++ client/src/components/icons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Icon} from 'react-mdl';
 
-const Check = () => <Icon name="check_circle" style={{color: '#4CAF50'}}/>;
-const Cross = () => <Icon name="cancel" style={{color: '#DE3226'}}/>;
+const Check = () => <Icon name="check_circle" className="valid-color"/>;
+const Cross = () => <Icon name="cancel" className="invalid-color"/>;
 
 export default {Check, Cross};
